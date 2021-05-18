2a1f57176c4bdf68b8ecbd843dbfbb2465f612ff
Dominik Zogg <dominik.zogg@gmail.com>
2021-05-18 21:54:38 +0200

    Revert "Do not use the router interface anymore, means code should run against 4.0-dev again"
    This reverts commit 5778d5236101501104f9d979747a84bf241836fc.

--- src/Router.php
+++ src/Router.php
@@ -6,6 +6,7 @@ namespace Chubbyphp\Framework\Router\FastRoute;
 
 use Chubbyphp\Framework\Router\RouteInterface;
 use Chubbyphp\Framework\Router\RouteMatcherInterface;
+use Chubbyphp\Framework\Router\RouterInterface;
 use Chubbyphp\Framework\Router\Routes;
 use Chubbyphp\Framework\Router\UrlGeneratorInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -13,7 +14,7 @@ use Psr\Http\Message\ServerRequestInterface;
 /**
  * @deprecated
  */
-final class Router implements RouteMatcherInterface, UrlGeneratorInterface
+final class Router implements RouterInterface
 {
     private RouteMatcherInterface $routeMatcher;
 
