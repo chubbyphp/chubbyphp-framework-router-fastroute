ae7fcd9e1d9e92acc78fa5d0850545f6e3a80a6c
Dominik Zogg <dominik.zogg@hotmail.com>
2022-02-28 17:04:13 +0100

    There is no need for imitate RouterInterface anymore, cause it doesn't exist since chubbyphp-framework 4.0

--- src/Router.php
+++ src/Router.php
@@ -2,69 +2,62 @@
 
 declare(strict_types=1);
 
-namespace Chubbyphp\Framework\Router {
-    interface RouterInterface extends RouteMatcherInterface, UrlGeneratorInterface
-    {
-    }
-}
+namespace Chubbyphp\Framework\Router\FastRoute;
 
-namespace Chubbyphp\Framework\Router\FastRoute {
-    use Chubbyphp\Framework\Router\RouteInterface;
-    use Chubbyphp\Framework\Router\RouteMatcherInterface;
-    use Chubbyphp\Framework\Router\RouterInterface;
-    use Chubbyphp\Framework\Router\Routes;
-    use Chubbyphp\Framework\Router\UrlGeneratorInterface;
-    use Psr\Http\Message\ServerRequestInterface;
+use Chubbyphp\Framework\Router\RouteInterface;
+use Chubbyphp\Framework\Router\RouteMatcherInterface;
+use Chubbyphp\Framework\Router\Routes;
+use Chubbyphp\Framework\Router\UrlGeneratorInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * @deprecated
+ */
+final class Router implements RouteMatcherInterface, UrlGeneratorInterface
+{
+    private RouteMatcherInterface $routeMatcher;
+
+    private UrlGeneratorInterface $urlGenerator;
 
     /**
-     * @deprecated
+     * @param array<RouteInterface> $routes
      */
-    final class Router implements RouterInterface
+    public function __construct(array $routes, ?string $cacheFile = null, string $basePath = '')
     {
-        private RouteMatcherInterface $routeMatcher;
+        @trigger_error(
+            sprintf('Use %s|%s instead of instead of "%s"', RouteMatcher::class, UrlGenerator::class, self::class),
+            E_USER_DEPRECATED
+        );
 
-        private UrlGeneratorInterface $urlGenerator;
+        $routes = new Routes($routes);
+        $this->routeMatcher = new RouteMatcher($routes, $cacheFile);
+        $this->urlGenerator = new UrlGenerator($routes, $basePath);
+    }
 
-        /**
-         * @param array<RouteInterface> $routes
-         */
-        public function __construct(array $routes, ?string $cacheFile = null, string $basePath = '')
-        {
-            @trigger_error(
-                sprintf('Use %s|%s instead of instead of "%s"', RouteMatcher::class, UrlGenerator::class, self::class),
-                E_USER_DEPRECATED
-            );
+    public function match(ServerRequestInterface $request): RouteInterface
+    {
+        return $this->routeMatcher->match($request);
+    }
 
-            $routes = new Routes($routes);
-            $this->routeMatcher = new RouteMatcher($routes, $cacheFile);
-            $this->urlGenerator = new UrlGenerator($routes, $basePath);
-        }
+    /**
+     * @param array<string, string> $attributes
+     * @param array<string, mixed>  $queryParams
+     */
+    public function generateUrl(
+        ServerRequestInterface $request,
+        string $name,
+        array $attributes = [],
+        array $queryParams = []
+    ): string {
+        return $this->urlGenerator->generateUrl($request, $name, $attributes, $queryParams);
+    }
 
-        public function match(ServerRequestInterface $request): RouteInterface
-        {
-            return $this->routeMatcher->match($request);
-        }
-
-        /**
-         * @param array<string, string> $attributes
-         * @param array<string, mixed>  $queryParams
-         */
-        public function generateUrl(
-            ServerRequestInterface $request,
-            string $name,
-            array $attributes = [],
-            array $queryParams = []
-        ): string {
-            return $this->urlGenerator->generateUrl($request, $name, $attributes, $queryParams);
-        }
-
-        /**
-         * @param array<string, string> $attributes
-         * @param array<string, mixed>  $queryParams
-         */
-        public function generatePath(string $name, array $attributes = [], array $queryParams = []): string
-        {
-            return $this->urlGenerator->generatePath($name, $attributes, $queryParams);
-        }
+    /**
+     * @param array<string, string> $attributes
+     * @param array<string, mixed>  $queryParams
+     */
+    public function generatePath(string $name, array $attributes = [], array $queryParams = []): string
+    {
+        return $this->urlGenerator->generatePath($name, $attributes, $queryParams);
     }
 }
